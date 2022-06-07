61aab58760d86f518daabb7f7af247a19cd5cf98
jonasmachados <jonasmachado.ti@gmail.com>
2022-06-07 11:01:49 -0300

    Created entities/Class User

---
+++ backend/src/main/java/com/Jonas/projectfullstack/entities/User.java
@@ -0,0 +1,68 @@
+package com.Jonas.projectfullstack.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "tb_first_name")
+    private String firstname;
+
+    @Column(name = "tb_last_name")
+    private String lastName;
+
+    private String email;
+
+    public User() {
+    }
+
+    public User(long id, String firstname, String lastName, String email) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
